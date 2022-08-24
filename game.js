c0187c8a7bd17e467aa43a50ba08b3c1547a3714
Belma Hadžić <bhadzic2@etf.unsa.ba>
2022-08-24 16:20:22 +0200

    sound function added

--- game.js
+++ game.js
@@ -1,12 +1,26 @@
 var buttonColours=["red", "blue", "green", "yellow"];
 var gamePattern=[];
+var userClickedPattern=[];
+
+
+$(".btn").click(function() {
+  var userChosenColour= $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  //console.log(userClickedPattern);
+});
 
 function nextSequence(){
   var randomNumber=Math.floor(Math.random()*4);
-  return randomNumber;
+  var randomChosenColour=buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
 }
 
-var randomChosenColour=buttonColours[nextSequence()];
-gamePattern.push(randomChosenColour);
-
-$("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+function playSound(name){
+  var audio=new Audio("sounds/"+ name +".mp3");
+  audio.play();
+}
