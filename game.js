110e4256670ac461a8fec5ba62cd5dea36fa40c9
Belma Hadžić <bhadzic2@etf.unsa.ba>
2022-08-24 16:48:46 +0200

    level update

--- game.js
+++ game.js
@@ -1,35 +1,45 @@
-var buttonColours=["red", "blue", "green", "yellow"];
-var gamePattern=[];
-var userClickedPattern=[];
-
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
 
 $(".btn").click(function() {
-  var userChosenColour= $(this).attr("id");
+  var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
   //console.log(userClickedPattern);
   animatePress(userChosenColour);
 });
 
-function nextSequence(){
-  var randomNumber=Math.floor(Math.random()*4);
-  var randomChosenColour=buttonColours[randomNumber];
+$(document).keydown(function() {
+  if (level == 0) {
+    $("#level-title").text("Level 0");
+    nextSequence();
+  }
+});
+
+
+function nextSequence() {
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
   playSound(randomChosenColour);
 }
 
-function playSound(name){
-  var audio=new Audio("sounds/"+ name +".mp3");
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
-function animatePress(currentColour){
-    $("#"+currentColour).addClass("pressed");
-  setTimeout(function () {
-    $("#"+currentColour).removeClass("pressed");
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
   }, 100);
 
 }
