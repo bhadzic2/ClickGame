ba4d3b6c9fe032a797c9ac5a2cdf116c71560c19
Belma Hadžić <bhadzic2@etf.unsa.ba>
2022-08-24 16:31:14 +0200

    Added animation and sounds on click

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ $(".btn").click(function() {
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
   //console.log(userClickedPattern);
+  animatePress(userChosenColour);
 });
 
 function nextSequence(){
@@ -23,4 +24,12 @@ function nextSequence(){
 function playSound(name){
   var audio=new Audio("sounds/"+ name +".mp3");
   audio.play();
+}
+
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#"+currentColour).removeClass("pressed");
+  }, 100);
+
 }
